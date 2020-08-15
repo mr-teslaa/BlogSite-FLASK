10b44e1d98fd54ce6ece68cd47ec87ade8df971e
Emon Hossain <hacker_tesla@protonmail.com>
2020-08-15 18:41:25 +0600

    adding some file

---
+++ flaskblog.py
@@ -0,0 +1,39 @@
+#importing necessary module
+from flask import Flask
+from flask import render_template
+
+
+posts =[
+    {
+        'author': 'tesla',
+        'title': 'first post',
+        'content': 'no more google, gmail, tracking. use duckduckgo.com',
+        'date_posted': '01 July 2020'
+    },
+    {
+        'author': 'sefuda',
+        'title': 'second post',
+        'content': 'no more youtube. use protonmail.com',
+        'date_posted': '01 July 2020'
+    }
+]
+
+
+#passing flask engine through 'app' variable
+app = Flask(__name__)
+
+#default route
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run()
